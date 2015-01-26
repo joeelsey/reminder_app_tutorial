b7b0a2195f8438d553db43730774f28c2ebe3063
Joe Elsey <joe.elsey@gmail.com>
2015-01-26 08:57:34 -0800

    clean up

--- cron.js
+++ cron.js
@@ -2,14 +2,13 @@
 
 module.exports = function(Reminder) {
   var client = require('twilio')(account, authToken);
-  var account = process.env.TWILIO_ACCOUNT_ID;
+  var account = process.env.TWILIO_ACCOUNT_SID;
   var authToken = process.env.TWILIO_AUTH_TOKEN;
   var Reminder = require('./models/reminder_model');
 
   function checkReminders() {
     Reminder.find({'end': { $lt: Date.now()}}, function(err, reminders) {
       console.log('finding jobs');
-      //console.log(reminders);
       if (err) return console.log('error: ', err);
       if (!reminders) return console.log('reminder not found');
 
@@ -18,7 +17,7 @@ module.exports = function(Reminder) {
         if(!reminder.isCompleted) {
           client.sms.messages.create({
             body: reminder.title,
-            to: '18474775286',
+            to: process.env.MOBILE,
             from: process.env.TWILIO_NUMBER
           }, function(err, message) {
             if (err) return console.log('error: ', err);
@@ -29,7 +28,6 @@ module.exports = function(Reminder) {
       });
     });
   };
-
   setInterval(checkReminders, 60000);
   checkReminders();
   console.log('check cron jobs');
--- time.js
+++ time.js
@@ -2,6 +2,7 @@ var moment = require('moment');
 
 function Time() {
   console.log(moment().add('hours').toDate());
+  console.log(process.env.MOBILE);
 };
 
 Time();
