bbc9800a5b720e544ff8b73333a2672370e20013
Joe Elsey <joe.elsey@gmail.com>
2015-01-26 13:20:14 -0800

    mocha tests green

--- test/api_test.js
+++ test/api_test.js
@@ -1,11 +1,13 @@
 'use strict';
 
-process.env.MONGO_URL = 'mongod://localhost/reminder_test';
+process.env.MONGO_URL = 'mongodb://localhost/reminder_test';
 var chai = require('chai');
 var chaihttp = require('chai-http');
 var Reminder = require('../models/reminder_model');
 
 chai.use(chaihttp);
+
+require('../server.js');
 
 var expect = chai.expect;
 var localhost = 'http://localhost:3000';
@@ -30,17 +32,6 @@ describe('test the reminder api', function() {
     }
   };
 
-  // //creates a message
-  // before(function(done) {
-  //   chai.request(localhost)
-  //     .post('/timers')
-  //     .send(message)
-  //     .end(function(err, res) {
-  //       if (err) return res.status(500).send(err);
-  //       done();
-  //     });
-  // });
-
   it('should create a message', function(done) {
     chai.request(localhost)
     .post('/timers')
@@ -59,7 +50,7 @@ describe('test the reminder api', function() {
     .get('/timers')
     .end(function(err, res) {
       expect(err).to.eql(null);
-      expect(Array.isArray(res.body)).to.eql(true);
+      expect(Array.isArray(res.body.timers)).to.eql(true);
       done();
     });
   });
@@ -69,7 +60,7 @@ describe('test the reminder api', function() {
     .get('/timers/' + id)
     .end(function(err, res) {
       expect(err).to.eql(null);
-      expect(res.body.id).to.equal(id);
+      expect(res.body._id).to.equal(id);
       expect(res.body).to.have.property('title');
       done();
     });
@@ -81,7 +72,8 @@ describe('test the reminder api', function() {
     .send(altmessage)
     .end(function(err, res) {
       expect(err).to.eql(null);
-      expect(res.body.title).to.equal('This is Message Three');
+      expect(res.body).to.have.property('timer');
+      expect(res.body.timer.title).to.equal('This is Message Three');
       done();
     });
   });
