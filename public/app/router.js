f0326bf60046c33f4b335419c51d6541445468f4
Joe Elsey <joe.elsey@gmail.com>
2015-01-29 14:32:00 -0800

    a little closer

--- public/app/router.js
+++ public/app/router.js
@@ -1,6 +1,6 @@
 Timers.Router.map(function() {
-  this.resource('timers', {path: '/index'}, function() {
-    this.route('active');
-    this.route('completed');
-  })
-})
+  this.resource('timers', {path: '/'}, function() {
+    this.resource('active');
+    this.resource('completed');
+  });
+});
--- public/app/store.js
+++ public/app/store.js
@@ -2,4 +2,3 @@ Timers.ApplicationSerializer = DS.LSSerializer.extend();
 Timers.ApplicationSerializer = DS.RESTSerializer.extend({
   primaryKey: '_id'
 });
-Timers.ApplicationAdapter = DS.LSAdapter.extend();
