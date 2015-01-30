06cd00a0da9cf8b8144cbf73f41a07f09f45b071
Joe Elsey <joe.elsey@gmail.com>
2015-01-29 17:43:37 -0800

    custom ds store works

--- public/app/store.js
+++ public/app/store.js
@@ -1,1 +1,14 @@
-Reminder.Store = DS.Store.extend();
+Reminder.Store = DS.Store.extend({
+  revision: 12,
+  adapter: Reminder.RESTAdapter
+});
+
+
+Reminder.RESTAdapter = DS.RESTAdapter.extend({
+  url: 'http://localhost:3000',
+  serializer: DS.RESTSerializer.extend({
+    primaryKey: function(type) {
+      return '_id'
+    }
+  })
+});
